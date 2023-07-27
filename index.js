f761849f0ac26675551e33b313a2a0ddb7428152
JimmyKurui <jimmy.chepkurui@strathmore.edu>
2023-07-28 01:22:04 +0300

    Proper event listening for keys. Attack boxes created

--- index.js
+++ index.js
@@ -4,36 +4,44 @@ const c = canvas.getContext('2d')
 canvas.width = 1024;
 canvas.height = 576;
 
-const gravity = 1
+const gravity = 0.7
 const keys = {
     // p1
-    'w' : isPressed = false,
-    'a' : isPressed = false,
-    's' : isPressed = false,
-    'd' : isPressed = false,
+    'w' : {isPressed : false},
+    'a' : {isPressed : false},
+    's' : {isPressed : false},
+    'd' : {isPressed : false},
     // p2
-    'ArrowUp' : isPressed = false,
-    'ArrowLeft' : isPressed = false,
-    'ArrowDown' : isPressed = false,
-    'ArrowRight' : isPressed = false,
+    'ArrowUp' : {isPressed : false},
+    'ArrowLeft' : {isPressed : false},
+    'ArrowDown' : {isPressed : false},
+    'ArrowRight' : {isPressed : false}
 }
 
 class Sprite {
-    constructor({position, velocity, color='red'}) {
+    constructor({position, velocity, color='red', offset}) {
         this.position = position
         this.velocity = velocity
         this.width = 50
         this.height = 100
         this.color = color
+        this.lastKey
         this.isAttacking = false
         this.attackBox = {
             position,
+            width : 100,
+            height : 50,
+            offset,
+            color : 'green'
         }
     }
 
     draw() {
         c.fillStyle = this.color
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        // Weapon
+        c.fillStyle = this.attackBox.color
+        c.fillRect(this.attackBox.position.x + this.attackBox.offset.x, this.attackBox.position.y + this.attackBox.offset.y, this.attackBox.width, this.attackBox.height)
     }
 
     update() {
@@ -49,6 +57,7 @@ class Sprite {
     }
 }
 
+// Players
 const p1 = new Sprite({
     position: {
         x: 100, y: 50
@@ -56,6 +65,9 @@ const p1 = new Sprite({
     velocity: {
         x: 0, y: 0
     },
+    offset: {
+        x: 0, y:0
+    }
 })
 
 const p2 = new Sprite({
@@ -65,6 +77,9 @@ const p2 = new Sprite({
     velocity: {
         x: 0, y: 0
     },
+    offset: {
+        x: -50, y:0
+    }
 })
 
 
@@ -75,6 +90,19 @@ function animate() {
     c.fillRect(0, 0, canvas.width, canvas.height)
     p1.update()
     p2.update()
+
+    // Free movement
+    if(keys.a.isPressed && p1.lastKey === 'a') {
+        p1.velocity.x = -5
+    } else if (keys.d.isPressed && p1.lastKey === 'd') {
+        p1.velocity.x = 5
+    }
+
+    if(keys.ArrowLeft.isPressed && p2.lastKey === 'ArrowLeft') {
+        p2.velocity.x = -5
+    } else if (keys.ArrowRight.isPressed && p2.lastKey === 'ArrowRight') {
+        p2.velocity.x = 5
+    } 
 }
 
 animate()
@@ -85,52 +113,74 @@ window.addEventListener('keydown', (event) => {
         case 'w':
             p1.velocity.y = -20
             keys.w.isPressed = true
+            p1.lastKey = 'w'
             break
         case 'a':
-            p1.velocity.x = -10
             keys.a.isPressed = true
+            p1.lastKey = 'a'
             break
         case 's':
             p1.isAttacking = true
             keys.s.isPressed = true
+            p1.lastKey = 's'
             break
         case 'd':
-            p1.velocity.x = 10
             keys.d.isPressed = true
+            p1.lastKey = 'd'
             break
         // Player 2
         case 'ArrowUp':
-            p2.velocity.y -= 20
+            p2.velocity.y = -20
             keys.ArrowUp.isPressed = true
+            p2.lastKey = 'ArrowUp'
             break
         case 'ArrowLeft':
-            p2.position.x -= 10
             keys.ArrowLeft.isPressed = true
+            p2.lastKey = 'ArrowLeft'
             break
         case 'ArrowDown':
             p2.isAttacking = true
             keys.ArrowDown.isPressed = true
+            p2.lastKey = 'ArrowDown'
             break
         case 'ArrowRight':
-            p2.velocity.x = 10
             keys.ArrowRight.isPressed = true
+            p2.lastKey = 'ArrowRight'
             break
     }
 })
 
 window.addEventListener('keyup', (event) => {
     switch(event.key) {
-        default:
-            p1.velocity.x = 0
-            p2.velocity.x = 0
+        case 'w':
             keys.w.isPressed = false
-            keys.a.isPressed = false
-            keys.s.isPressed = false
-            keys.d.isPressed = false
-            keys.ArrowUp.isPressed = false
-            keys.ArrowLeft.isPressed = false
-            keys.ArrowDown.isPressed = false
-            keys.ArrowRight.isPressed = false
             break
+        case 'a':
+            p1.velocity.x = 0
+            keys.a.isPressed = false
+            break
+        case 's':
+            keys.s.isPressed = false
+            break
+        case 'd':
+            p1.velocity.x = 0
+            keys.d.isPressed = false
+            break
+        default:
+            // p1.velocity.x = 0
+            case 'ArrowUp':
+                keys.ArrowUp.isPressed = false
+                break
+            case 'ArrowLeft':
+                p2.velocity.x = 0
+                keys.ArrowLeft.isPressed = false
+                break
+            case 'ArrowDown':
+                keys.ArrowDown.isPressed = false
+                break
+            case 'ArrowRight':
+                p2.velocity.x = 0
+                keys.ArrowRight.isPressed = false
+                break
     }
 })
